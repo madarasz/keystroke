f7512873451b3b778294aa41b898e62329fdd4da
madarasz <Sajt123*>
2017-06-15 15:47:55 +0200

    adding mouse logging, 3 mouse charts

--- chart.js
+++ chart.js
@@ -1,8 +1,9 @@
-google.charts.load("current", {packages:["timeline"]});
+google.charts.load("current", {packages:["timeline", "corechart"]});
 
-function drawChart() {
+// draw keyboard timeline
+function drawKeyboardChart() {
 
-    var container = document.getElementById('chart');
+    var container = document.getElementById('chart-keyboard');
     var chart = new google.visualization.Timeline(container);
     var dataTable = new google.visualization.DataTable();
     dataTable.addColumn({ type: 'string', id: 'Char' });
@@ -21,8 +22,91 @@ function drawChart() {
         timeline: {
             groupByRowLabel: true,
             singleColor: 'red'
-        }
+        },
+        height: 500
     };
 
     chart.draw(dataTable, options);
+    container.classList.remove('hidden-xs-up'); // make chart visible
+}
+
+// draws mouse charts
+function drawMouseCharts() {
+
+    var prevX, prevY, prevTimestamp, speed, prevSpeed,
+        // chart containers
+        containerMovement = document.getElementById('chart-mouse-movement'),
+        containerSpeed = document.getElementById('chart-mouse-speed'),
+        containerAcceleration = document.getElementById('chart-mouse-acceleration'),
+        // charts
+        chartMovement = new google.visualization.ScatterChart(containerMovement),
+        chartSpeed = new google.visualization.LineChart(containerSpeed),
+        chartAcceleration = new google.visualization.LineChart(containerAcceleration),
+        // data tables
+        dataTableMovement = new google.visualization.DataTable(),
+        dataTableSpeed = new google.visualization.DataTable(),
+        dataTableAcceleration = new google.visualization.DataTable();
+
+    // add data table columns
+    dataTableMovement.addColumn({ type: 'number', id: 'x' });
+    dataTableMovement.addColumn({ type: 'number', id: 'y' });
+    dataTableMovement.addColumn({ type: 'string', role: 'style' }); // to indicate clicks with different color
+    dataTableSpeed.addColumn({ type: 'number', id: 'time' });
+    dataTableSpeed.addColumn({ type: 'number', id: 'speed' });
+    dataTableAcceleration.addColumn({ type: 'number', id: 'time' });
+    dataTableAcceleration.addColumn({ type: 'number', id: 'acceleration' });
+
+    // iterate over mouse log
+    for (var i = 0; i < mouseLog.length; i++) {
+
+        // movement data
+        if (mouseLog[i].type == 'movement') {
+
+            dataTableMovement.addRow([mouseLog[i].x, mouseLog[i].y, null]);
+            // speed data
+            if (i > 0) {
+                // speed = distance / time passed
+                speed = Math.sqrt(Math.pow(mouseLog[i].x - prevX, 2) + Math.pow(mouseLog[i].x - prevY, 2))
+                            / (mouseLog[i].timestamp - prevTimestamp);
+
+                dataTableSpeed.addRow([mouseLog[i].timestamp, speed]);
+            }
+            // acceleration data
+            if (i > 1) {
+                dataTableAcceleration.addRow([mouseLog[i].timestamp, speed - prevSpeed]);
+            }
+            // storing previous values
+            prevX = mouseLog[i].x; prevY = mouseLog[i].y; prevTimestamp = mouseLog[i].timestamp; prevSpeed = speed;
+
+        }
+
+    }
+
+    // iterate for mouse clicks, so they are on top
+    for (var i = 0; i < mouseLog.length; i++) {
+        if (mouseLog[i].type == 'mouseup') {
+            dataTableMovement.addRow([mouseLog[i].x, mouseLog[i].y, 'point { fill-color: #ff0000 }']);  // mark with red dot
+        }
+    }
+
+    var optionsMovement = {
+            title: 'Mouse movement',
+            hAxis: { minValue: 0 },
+            vAxis: { minValue: 0, direction: -1 },
+            legend: 'none',
+            height: 500,
+            width: 500
+        },
+        optionsSpeed = {
+            title: 'Mouse speed',
+            legend: 'none'
+        },
+        optionsAcceleration = {
+            title: 'Mouse acceleration',
+            legend: 'none'
+        };
+
+    chartMovement.draw(dataTableMovement, optionsMovement);
+    chartSpeed.draw(dataTableSpeed, optionsSpeed);
+    chartAcceleration.draw(dataTableAcceleration, optionsAcceleration);
 }
--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 var startTime = Date.now(),
     beingPressed = [], // contains keys currently being pressed: keycode, keyDownTimestamp
-    keystrokeLog = []; // contains keystrokes logged: keycode, keyDownTimestamp, keyUpTimestamp
+    keystrokeLog = [], // contains keystrokes logged: keycode, keyDownTimestamp, keyUpTimestamp
+    mouseLog = [];     // contains mouse movements logged: type (movement, buttonDown, buttonUp), x, y, timestamp, [mousecode]
 
-document.addEventListener('keydown', keyDownEvent, true);
-document.addEventListener('keyup', keyUpEvent, true);
+document.addEventListener('keydown', keyDownEvent, false);
+document.addEventListener('keyup', keyUpEvent, false);
+window.onmousemove = mouseMoveEvent;
+document.addEventListener('mousedown', mouseButtonEvent, false);
+document.addEventListener('mouseup', mouseButtonEvent, false);
 
-function keyDownEvent(e) {
+// when a key pressed down
+function keyDownEvent(event) {
     var timeStamp = Date.now() - startTime;
 
     // if not being pressed down before
-    if (getIndex(e.which, beingPressed) == -1) {
+    if (getIndex(event.which, beingPressed) == -1) {
         // store in beingPressed
-        beingPressed.push({ keycode: e.which, keyDownTimestamp: timeStamp });
+        beingPressed.push({ keycode: event.which, keyDownTimestamp: timeStamp });
     }
 
 }
 
-function keyUpEvent(e) {
+// when a key is let go
+function keyUpEvent(event) {
     var timeStamp = Date.now() - startTime,
-        index = getIndex(e.which, beingPressed);
+        index = getIndex(event.which, beingPressed);
 
     // error handling, key was not pushed before
     if (index == -1) {
-        console.log('WARN: key was not down before, code: ' + e.which);
-        keystrokeLog.push({ keycode: e.which, keyDownTimestamp: -1, keyUpTimestamp: timeStamp });
+        console.log('WARN: key was not down before, code: ' + event.which);
+        keystrokeLog.push({ keycode: event.which, keyDownTimestamp: -1, keyUpTimestamp: timeStamp });
         return false;
     }
 
@@ -35,6 +41,25 @@ function keyUpEvent(e) {
     console.log('"' + keyboardMap[beingPressed[index].keycode] +'"', beingPressed[index]);
     // delete from beingPressed
     beingPressed.splice(index, 1);
+}
+
+// when the mouse is moved
+function mouseMoveEvent(event) {
+    event = event || window.event; // handle IE
+
+    var timeStamp = Date.now() - startTime,
+        movement = { type: 'movement', x: event.pageX, y: event.pageY, timestamp: timeStamp };
+
+    mouseLog.push(movement);
+    console.log(movement);
+}
+
+function mouseButtonEvent(event) {
+    var timeStamp = Date.now() - startTime,
+        click = { type: event.type, x: event.pageX, y: event.pageY, timestamp: timeStamp, mousecode: event.button };
+
+    mouseLog.push(click);
+    console.log(click);
 }
 
 // gives back index of keycode in array
